93a0ae42ab939f2df06136b8511678a66cc22257
xeobass <mazaroca_de_millo@hotmail.com>
2017-12-20 11:53:41 +0100

    Initial commit

---
+++ Lesson01_HelloWorld/app/src/main/java/com/learning/nelson/lesson01_helloworld/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.learning.nelson.lesson01_helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
