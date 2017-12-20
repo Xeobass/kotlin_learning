d5b003350943adec9f57f88a3b6e56972487a145
xeobass <mazaroca_de_millo@hotmail.com>
2017-12-20 12:34:15 +0100

    added anko lib and other stuff. Show the message set with the text property

--- Lesson01_HelloWorld/app/src/main/java/com/learning/nelson/lesson01_helloworld/MainActivity.kt
+++ Lesson01_HelloWorld/app/src/main/java/com/learning/nelson/lesson01_helloworld/MainActivity.kt
@@ -2,12 +2,32 @@ package com.learning.nelson.lesson01_helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        message.text = "Hello Kotlin!"
+
+        /*
+        * What i did?
+        *
+        * Added the Anko lib to get more powerfull features (check build.grade to see how)
+        * Added the main string to strings.xml file (check app/values/strings.xml)
+        * Created a TextView component and set id 'message' (now...the size is not a problem)
+        * with Kotlin, I access directly to the component just type the id ('message')
+        * and set a text with the 'text' property =)
+        *
+        * Kotlin give us the power to access to the view without looking for it (findElementById()...R.Id.bla bla bla)
+        * or using others third party libs. All comes with Kotlin!
+        *
+        *
+        * */
+
     }
 
 }
